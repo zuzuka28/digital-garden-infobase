46800bb12b477c353aa3527f30a026b62e30118b
zuzuka28 <zuzuka28kavaii@gmail.com>
2024-05-16 16:43:31 +0300

    upd

--- quartz.config.ts
+++ quartz.config.ts
@@ -67,7 +67,7 @@ const config: QuartzConfig = {
       Plugin.ObsidianFlavoredMarkdown({ enableInHtmlEmbed: false }),
       Plugin.GitHubFlavoredMarkdown(),
       Plugin.TableOfContents(),
-      Plugin.CrawlLinks({ markdownLinkResolution: "absolute" }),
+      Plugin.CrawlLinks({ markdownLinkResolution: "shortest" }),
       Plugin.Description(),
     ],
     filters: [Plugin.RemoveDrafts()],
